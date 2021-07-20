1a1959a7eed59dbbb59d563ecb2a1d7053beee50
grom48 <viktorbilokapicm@gmail.com>
2021-07-20 16:41:49 +0200

    promjena

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,26 +102,22 @@ sliderBtnLeft.addEventListener("click", () => {
 startSlider();
 
 
-let scrollToTopBtn = document.getElementById("scrollToTopBtn")
+//Get the button:
+let mybutton = document.getElementById("mybtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    scrollToTopBtn.style.display = "block";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    scrollToTopBtn.style.display = "none";
+    mybutton.style.display = "none";
   }
 }
 
-window.onscroll = function() {scrollFunction()};
-
-let rootElement = document.documentElement
-
-function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-scrollToTopBtn.addEventListener("click", scrollToTop);
-
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
